adb9e7b0ed07650daa40b7a0d700f98a0b596805
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-11-24 09:05:49 +0000

    add serializer for profiles and list view

--- profiles/models.py
+++ profiles/models.py
@@ -6,18 +6,19 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_mhsqxg'
+        upload_to='images/', default='../default_profile_qdjgyp'
     )
 
-class Meta:
-    ordering = ['-created_at']
+    class Meta:
+        ordering = ['-created_at']
 
-def __str__(self):
-    return f"{self.owner}'s profile"
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
