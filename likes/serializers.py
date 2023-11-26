741c3f23200c1aec0dc2f7d97b43c107f775a7af
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-11-26 11:14:49 +0000

    created LikeList and LikeDetail generic views

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from likes.models import Like
 
@@ -5,15 +6,14 @@ from likes.models import Like
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
 
-class Meta:
-    model = Like
-    fields = ['id', 'created_at', 'owner', 'post']
-
-def create(self, validated_data):
-    try:
-        return super().create(validated_data)
-    except IntregrityError:
-        raise serializers.ValidationError({
-            'detail': 'possible duplicate'
-        })
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntregrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
