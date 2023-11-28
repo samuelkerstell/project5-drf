84825b3b4981ac99b7051ebb8733037996c69697
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-11-28 16:59:55 +0000

    heroku deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -62,7 +63,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '8000-samuelkerst-project5drf-y56ppogm9tb.ws-eu106.gitpod.io', 'project-portfolio-5-sam-7342558136fc.herokuapp.com', 'project-portfolio-5-sam.herokuapp.com' ]
+ALLOWED_HOSTS = [
+    'localhost', 
+    '8000-samuelkerst-project5drf-y56ppogm9tb.ws-eu106.gitpod.io', 
+    os.environ.get('ALLOWED_HOST'), 
+    'project-portfolio-5-sam.herokuapp.com',
+]
 
 
 # Application definition
@@ -108,9 +114,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
