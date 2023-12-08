9d2b1496939198f6ad421672588fae3aac5a3ce1
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-08 20:23:59 +0000

    update followers app

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,8 +1,13 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from followers.models import Follower
+from .models import Follower
+
 
 class FollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Follower model
+    Create method handles the unique constraint on 'owner' and 'followed'
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     followed_name = serializers.ReadOnlyField(source='followed.username')
 
@@ -15,7 +20,5 @@ class FollowerSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         try:
             return super().create(validated_data)
-        except IntregrityError:
-            raise serializers.ValidationError({
-                'detail': 'possible duplicate'
-            })
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
