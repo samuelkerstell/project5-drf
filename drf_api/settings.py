dc4311922c413d6340af0436cf64cdcea2043bc7
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-14 09:58:55 +0000

    update debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,8 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'localhost', 
