a6f5ef634739736f4ef6123872334b3fccb59f18
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-16 20:42:32 +0000

    update: cors

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -115,14 +115,17 @@ MIDDLEWARE = [
 ]
 
 #if 'CLIENT_ORIGIN_DEV' in os.environ:
-#    CORS_ALLOWED_ORIGINS = [
-#        os.environ.get('CLIENT_ORIGIN')
+#    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+#    CORS_ALLOWED_ORIGIN_REGEXES = [
+#        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
 #    ]
 
+
+
+# Add this before deployment
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
